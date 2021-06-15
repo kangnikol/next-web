94219c094113565abb248f80f157f9a928e3a15e
Pramudya Arya Wicaksana <arya@Cliffords-Mac-mini.local>
2021-06-15 10:16:00 +0700

    Init Arya

---
+++ components/About.js
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <div className="aboutRoot">
+            <div className="container">
+                <div className="row">
+                    <div className="col">  
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
