0de60f45cc50dc25322e1978318e08678a6549f9
kangnikol <nicholasalvisaputra@gmail.com>
2021-06-17 17:49:48 +0700

    editing footer

--- components/Footer.js
+++ components/Footer.js
@@ -7,20 +7,18 @@ const Footer = () => {
           {/* <!-- flex-container start --> */}
           <div class="container flex-align-center">
             {/* <!-- column start --> */}
-            <div class="eight-columns padding-top-90">
-              <h4 class="column-l-r-margin-10 headline-s headline-uppercase">
+            <div class="row fRows">
+              <h4 class="col-10">
                 The Best
                 <br />
                 Agency for
                 <br />
                 Your Businnes
               </h4>
-            </div>
-            {/* <!-- column end --> */}
+              {/* <!-- column end --> */}
 
-            {/* <!-- column start --> */}
-            <div class="four-columns padding-top-90">
-              <ul class="column-l-r-margin-10 list">
+              {/* <!-- column start --> */}
+              <div class="col">
                 <li class="list__item">
                   <a
                     href="index.html"
@@ -69,182 +67,122 @@ const Footer = () => {
                     Contact
                   </a>
                 </li>
-              </ul>
+              </div>
             </div>
             {/* <!-- column end --> */}
           </div>
           {/* <!-- flex-container end --> */}
 
           {/* <!-- flex-container start --> */}
-          <div class="flex-container padding-top-30">
-            {/* <!-- column start --> */}
-            <div class="four-columns padding-top-60">
-              <div class="column-l-r-margin-10">
-                <div class="headline-xxxxs">Email:</div>
-                <div class="margin-top-20">
-                  <a href="#" class="line-btn js-pointer-small">
-                    email@tumar_agency.com
-                  </a>
-                  <div class="margin-top-5">
-                    <a href="#" class="line-btn js-pointer-small">
-                      email@tumar_job.com
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* <!-- column end --> */}
-
-            {/* <!-- column start --> */}
-            <div class="four-columns padding-top-60">
-              <div class="column-l-r-margin-10">
-                <div class="headline-xxxxs">Phone:</div>
-                <div class="margin-top-20">
-                  <a
-                    href="#"
-                    class="
-                        subhead-xxs
-                        text-color-b0b0b0 text-hover-to-white
-                        js-pointer-small
-                      "
-                  >
-                    +23 8 8532 7834
-                  </a>
-                  <div class="margin-top-5">
-                    <a
-                      href="#"
-                      class="
-                          subhead-xxs
-                          text-color-b0b0b0 text-hover-to-white
-                          js-pointer-small
-                        "
-                    >
-                      +23 8 9635 7335
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            {/* <!-- column end --> */}
-
-            {/* <!-- column start --> */}
-            <div class="four-columns padding-top-60">
-              <div class="column-l-r-margin-10">
-                <div class="headline-xxxxs">Address:</div>
-                <div class="margin-top-20">
-                  <a
-                    href="#"
-                    class="
-                        subhead-xxs
-                        text-color-b0b0b0 text-hover-to-white
-                        js-pointer-small
-                      "
-                  >
-                    PO Box 223158 Oliver Street
-                    <br />
-                    East Victoria 2006 UK
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <!-- column end --> */}
-
-            {/* <!-- column start --> */}
-            <div class="twelve-columns padding-top-90">
-              <div class="column-l-r-margin-10 padding-top-30 pos-rel">
-                <div
-                  class="anim-line-top"
-                  data-scroll
-                  data-scroll-offset="5%"
-                ></div>
-                {/* <!-- footer-social start --> */}
-                <ul class="list list_row list_margin-30px">
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Facebook
-                    </a>
-                  </li>
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Dribbble
-                    </a>
-                  </li>
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Instagram
-                    </a>
-                  </li>
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Youtube
-                    </a>
-                  </li>
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Behance
-                    </a>
-                  </li>
-                  <li class="list__item">
-                    <a
-                      href="#"
-                      class="flip-btn js-pointer-small"
-                      data-splitting
-                    >
-                      Twitter
-                    </a>
-                  </li>
-                </ul>
-                {/* <!-- footer-social end --> */}
-              </div>
-            </div>
-            {/* <!-- column end --> */}
-
-            {/* <!-- column start --> */}
-            <div class="twelve-columns padding-top-120">
-              <div class="column-l-r-margin-10 pos-rel">
-                <p class="copyright margin-right-30">
-                  &copy; Copyright 2020 TUMAR. Template by
-                  <a href="#" class="copyright__author js-pointer-small">
-                    Jinna Gik
-                  </a>
-                </p>
-                {/* <!-- to top btn start --> */}
-                <a
-                  href="#up"
-                  class="scroll-to-btn js-pointer-large"
-                  data-scroll
-                  data-scroll-repeat
-                  data-scroll-to
-                >
-                  <span class="scroll-to-btn__arrow"></span>{" "}
+          {/* <!-- column start --> */}
+          <div class="row fRows2">
+            <div class="col">
+              <h5>Email:</h5>
+              <p className="contact">
+                <a href="#" class="email">
+                  email@tumar_agency.com
                 </a>
-                {/* <!-- to top btn end --> */}
-              </div>
+              </p>
+              <p className="contact">
+                <a href="#" className="email">
+                  email@tumar_job.com
+                </a>
+              </p>
             </div>
             {/* <!-- column end --> */}
+
+            {/* <!-- column start --> */}
+            <div class="col">
+              <h5>Phone:</h5>
+              <p className="contact">
+                <a href="#">+23 8 8532 7834</a>
+              </p>
+              <p className="contact">
+                <a href="#">+23 8 9635 7335</a>
+              </p>
+            </div>
+            {/* <!-- column end --> */}
+
+            {/* <!-- column start --> */}
+            <div class="col">
+              <h5>Address:</h5>
+              <p className="contact">
+                <a href="#">
+                  PO Box 223158 Oliver Street East Victoria 2006 UK
+                </a>
+              </p>
+            </div>
           </div>
-          {/* <!-- flex-container end --> */}
+          {/* <!-- column end --> */}
+
+          {/* <!-- column start --> */}
+          <div class="row fRows3">
+            <div class="col">
+              {/* <!-- footer-social start --> */}
+              <ul class="list list_row list_margin-30px">
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Facebook
+                  </a>
+                </li>
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Dribbble
+                  </a>
+                </li>
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Instagram
+                  </a>
+                </li>
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Youtube
+                  </a>
+                </li>
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Behance
+                  </a>
+                </li>
+                <li class="list__item">
+                  <a href="#" class="flip-btn js-pointer-small" data-splitting>
+                    Twitter
+                  </a>
+                </li>
+              </ul>
+              {/* <!-- footer-social end --> */}
+            </div>
+          </div>
         </div>
+        {/* <!-- column end --> */}
+
+        {/* <!-- column start --> */}
+        <div class="twelve-columns padding-top-120">
+          <div class="column-l-r-margin-10 pos-rel">
+            <p class="copyright margin-right-30">
+              &copy; Copyright 2020 TUMAR. Template by
+              <a href="#" class="copyright__author js-pointer-small">
+                Jinna Gik
+              </a>
+            </p>
+            {/* <!-- to top btn start --> */}
+            <a
+              href="#up"
+              class="scroll-to-btn js-pointer-large"
+              data-scroll
+              data-scroll-repeat
+              data-scroll-to
+            >
+              <span class="scroll-to-btn__arrow"></span>{" "}
+            </a>
+            {/* <!-- to top btn end --> */}
+          </div>
+        </div>
+        {/* <!-- column end --> */}
+
+        {/* <!-- flex-container end --> */}
+        {/* </div> */}
         {/* <!-- container end --> */}
       </footer>
       {/* <!-- footer end --> */}
