a7e51f4f9ef8e5b534f4e811719feca40da90dc5
kangnikol <nicholasalvisaputra@gmail.com>
2021-06-22 15:58:05 +0700

    marquee section

--- pages/content/Marquee.js
+++ pages/content/Marquee.js
@@ -1,10 +1,29 @@
+import Image from "next/image";
+
 const Marquee = () => {
   return (
     <div className="marqueeRoot">
       <div className="container">
-        <div className="row">
+        <div className="row content">
           <div className="col">
-            <h1>We deliver</h1>
+            <h1>We wonder</h1>
+          </div>
+          <div className="col">
+            <Image
+              src="/wonder.png"
+              href="https://www.joinwonder.com/"
+              width={236}
+              height={128}
+            />
+          </div>
+          <div className="col">
+            <p>
+              We are part of the fast growing Wonder Networks. Senja is
+              Indonesias partner and is providing staffing and offshoring
+              solutions to startups, agencies, organisations and individuals
+              around the world. Visit our Wonder today and get started building
+              a highly talented and cost-efficient team of experts.
+            </p>
           </div>
         </div>
       </div>
