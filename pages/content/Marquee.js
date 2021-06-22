ebfdcd6efca0d1d82685a00c96da3f99536cc33e
kangnikol <nicholasalvisaputra@gmail.com>
2021-06-22 14:39:13 +0700

    add container

--- pages/content/Marquee.js
+++ pages/content/Marquee.js
@@ -1,9 +1,11 @@
 const Marquee = () => {
   return (
     <div className="marqueeRoot">
-      <div className="row">
-        <div className="col">
-          <h1>We deliver</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>We deliver</h1>
+          </div>
         </div>
       </div>
     </div>
